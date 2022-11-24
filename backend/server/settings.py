5715b8895c7760bb5f99126ac539bd6fc8f23099
Alexandr S <alexandr@raisesoftware.com>
2022-11-24 11:15:35 +0700

    Update settings.py and .env.example files for correct .env paths.

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -14,19 +14,28 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Setup environment variables
+DOTENV_DEFAULT_CONFIG = '.env.example'
+DOTENV_CONFIG = '.env'
+
+if os.path.exists(BASE_DIR / DOTENV_CONFIG):
+    DOTENV_DEFAULT_CONFIG = DOTENV_CONFIG
+
+# Enable environment variables
+load_dotenv(DOTENV_DEFAULT_CONFIG, override=True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "")
+SECRET_KEY = os.getenv("SECRET_KEY", "value does not exist")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get("DEBUG", "0")))
+DEBUG = os.getenv("DEBUG", False)
 
 ALLOWED_HOSTS = ["*"]
 CORS_ORIGIN_ALLOW_ALL = True
