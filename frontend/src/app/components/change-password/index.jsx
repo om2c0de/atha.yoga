0d631c63856fd7171591cd4cfcf70acc1c7e0f42
Xenia Gorbunova <Gorbunovaksenia@gmail.com>
2022-12-07 16:35:27 +0300

    Update index.jsx

--- frontend/src/app/components/change-password/index.jsx
+++ frontend/src/app/components/change-password/index.jsx
@@ -43,54 +43,55 @@ const ChangePassword = () => {
   };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Typography component="h1" variant="h4" sx={{ mb: 3 }}>
-          Изменить пароль
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }} className="form__container">
-          <OutlinedInput
-            sx={{ mb: 2, mt: 1 }}
-            fullWidth
-            label="Новый пароль"
-            name="password"
-            placeholder="Пароль"
-            id="password"
-            autoComplete="current-password"
-            type={values.showPassword ? 'text' : 'password'}
-            value={values.password}
-            onChange={handleChange('password')}
-            endAdornment={(
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                >
-                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            )}
-          />
-          <Button
-            type="submit"
-            size="large"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 2, mb: 1 }}
-          >
-            Изменить
-          </Button>
+    <div className="container-center">
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h4" sx={{ mb: 3 }}>
+            Изменить пароль
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }} className="form__container">
+            <OutlinedInput
+              sx={{ mb: 2, mt: 1 }}
+              fullWidth
+              label="Новый пароль"
+              name="password"
+              placeholder="Пароль"
+              id="password"
+              autoComplete="current-password"
+              type={values.showPassword ? 'text' : 'password'}
+              value={values.password}
+              onChange={handleChange('password')}
+              endAdornment={(
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              )}
+            />
+            <Button
+              type="submit"
+              size="large"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 2, mb: 1 }}
+            >
+              Изменить
+            </Button>
+          </Box>
         </Box>
-      </Box>
-    </Container>
+      </Container>
+    </div>
   );
 };
 export default ChangePassword;
