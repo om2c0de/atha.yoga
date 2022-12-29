e7bacf19f434133f704a001c34346dfafd9b790e
chudo <Vse21v@gmail.com>
2022-12-29 12:29:55 +0300

    resolve conflict

--- backend/courses/models.py
+++ backend/courses/models.py
@@ -57,7 +57,7 @@ class Course(TimeStampedModel):
     name = models.CharField(max_length=64)
     description = models.TextField()
     course_type = models.CharField(max_length=30, choices=CourseTypes.choices)
-    level = models.CharField(max_length=30)
+    level = models.CharField(max_length=40)
     single = models.BooleanField(default=False)
     duration = models.DurationField()
     start_datetime = models.DateTimeField()
